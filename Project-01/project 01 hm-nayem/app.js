3314eb6824ccf18d3ae3931e832adb687785a7e0
abdul-aziz <tamimaziz2007@gmail.com>
2024-07-09 18:09:32 +0600

    update

--- Project-01/project 01 hm-nayem/app.js
+++ Project-01/project 01 hm-nayem/app.js
@@ -1,34 +1,33 @@
 /**
-    *Project Requirements:
-    * -Change the background color by generating random rgb color by clicking a button
+ *Project Requirements:
+ * -Change the background color by generating random rgb color by clicking a button
  */
 
 // Steps
 
 // Step 1 - create onload function
 window.onload = () => {
-    main();
-}
+  main();
+};
 
 function main() {
-    const root = document.getElementById('root')
-    const btn = document.getElementById('change-btn')
+  const root = document.getElementById("root");
+  const btn = document.getElementById("change-btn");
 
-    btn.addEventListener('click', function () {
-        const bgColor = genarateRGBColor()
-        root.style.backgroundColor = bgColor;
-    })
+  btn.addEventListener("click", function () {
+    const bgColor = genarateRGBColor();
+    root.style.backgroundColor = bgColor;
+  });
 }
 // Step 2 - random color generator function
 function genarateRGBColor() {
-    // rgb(0, 0, 0). rgb(255, 255, 255)
-    const red = Math.floor(Math.random() * 255);
-    const green = Math.floor(Math.random() * 255);
-    const blue = Math.floor(Math.random() * 255);
+  // rgb(0, 0, 0). rgb(255, 255, 255)
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
 
-    return `rgb(${red}, ${green}, ${blue})`
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 // Step 3 - collect all necessary references
 
 // Step 4 - handle the click event
-
