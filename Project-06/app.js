2d1791c6a64bee2e7a3de65c90a9e026b4c07770
abdul-aziz <tamimaziz2007@gmail.com>
2024-07-14 15:21:56 +0600

    update

--- Project-06/app.js
+++ Project-06/app.js
@@ -5,88 +5,87 @@
  * - Add a button to copy the color code
  * - Add a toast message when copied
  * - User can type their own hex code too
-*/
-
+ */
 
 // Globals
 let div = null;
 
 // Step 1 - create onload handler
 window.onload = () => {
-    main();
+  main();
 };
 
 function main() {
-    const root = document.getElementById('root');
-    const output = document.getElementById('output');
-    const changeBtn = document.getElementById('change-btn');
-    const copyBtn = document.getElementById('copy-btn');
+  const root = document.getElementById("root");
+  const output = document.getElementById("output");
+  const changeBtn = document.getElementById("change-btn");
+  const copyBtn = document.getElementById("copy-btn");
 
-    changeBtn.addEventListener('click', function () {
-        const bgColor = generateHexColor();
-        root.style.backgroundColor = bgColor;
-        output.value = bgColor.substring(1);
-    });
+  changeBtn.addEventListener("click", function () {
+    const bgColor = generateHexColor();
+    root.style.backgroundColor = bgColor;
+    output.value = bgColor.substring(1);
+  });
 
-    copyBtn.addEventListener('click', function () {
-        navigator.clipboard.writeText(`#${output.value}`);
-        if (div !== null) {
-            div.remove();
-            div = null;
-        }
-        if (isValidHex(output.value)) {
-            generateToastMessage(`#${output.value} copied`);
-        }  else {
-            alert('Invalid color code');
-        }
-    });
+  copyBtn.addEventListener("click", function () {
+    navigator.clipboard.writeText(`#${output.value}`);
+    if (div !== null) {
+      div.remove();
+      div = null;
+    }
+    if (isValidHex(output.value)) {
+      generateToastMessage(`#${output.value} copied`);
+    } else {
+      alert("Invalid color code");
+    }
+  });
 
-    output.addEventListener('keyup', function (e) {
-        const color = e.target.value;
-        if (color) {
-            output.value = color.toUpperCase();
-            if (isValidHex(color)) {
-                root.style.background = `#${color}`;
-            }
-        }
-    });
+  output.addEventListener("keyup", function (e) {
+    const color = e.target.value;
+    if (color) {
+      output.value = color.toUpperCase();
+      if (isValidHex(color)) {
+        root.style.background = `#${color}`;
+      }
+    }
+  });
 }
 
 // step 2 - random color generator function
 function generateHexColor() {
-    // #000000 #ffffff
-    // 255, 255, 255, -> #FFFFFF
-    const red = Math.floor(Math.random() * 255);
-    const green = Math.floor(Math.random() * 255);
-    const blue = Math.floor(Math.random() * 255);
+  // #000000 #ffffff
+  // 255, 255, 255, -> #FFFFFF
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
 
-    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
 
 function generateToastMessage(msg) {
-    div = document.createElement('div');
-    div.innerHTML = msg;
-    div.className = 'toast-message toast-message-slide-in';
+  div = document.createElement("div");
+  div.innerHTML = msg;
+  div.className = "toast-message toast-message-slide-in";
 
-    div.addEventListener('click', function () {
-        div.classList.remove('toast-message-slide-in');
-        div.classList.add('toast-message-slide-out');
+  div.addEventListener("click", function () {
+    div.classList.remove("toast-message-slide-in");
+    div.classList.add("toast-message-slide-out");
 
-        div.addEventListener('animationend', function () {
-            div.remove();
-            div = null;
-        });
+    div.addEventListener("animationend", function () {
+      div.remove();
+      div = null;
     });
+  });
 
-    document.body.appendChild(div);
+  document.body.appendChild(div);
 }
 
 /**
  * @param {string} color : ;
  */
 function isValidHex(color) {
-    if (color.length !== 6) return false;
-    return /^[0-9A-Fa-f]{6}$/i.test(color);
+  if (color.length !== 6) return false;
+  return /^[0-9A-Fa-f]{6}$/i.test(color);
 }
 
 // step 3 - collect all necessary references
